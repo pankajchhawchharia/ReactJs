843dfc192d8f8f547f84f585dcff1f484d27f5bd
Pankaj Chhawchharia <pankaj05011994@gmail.com>
2024-12-08 12:18:52 +0530

    Create App.js
    Render Components in App

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ComponentA from './ComponentA';
+import ComponentB from './ComponentB';
+
+const App = () => {
+  return (
+    <div>
+      <ComponentA />
+      <ComponentB />
+    </div>
+  );
+};
+
+export default App;
