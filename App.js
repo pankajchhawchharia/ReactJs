d9eeda8a4d0b9003213f194979f1b4ed55d37808
Pankaj Chhawchharia <pankaj05011994@gmail.com>
2024-12-09 13:18:06 +0530

    Create App.js react lazy
    react lazy

---
+++ App.js
@@ -0,0 +1,19 @@
+import React,{Suspense} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+const Home = React.lazy(()=> import ('./Home');
+const About = React.lazy(() => import ('./About');
+
+function App () {
+  return (
+    <Router>
+    <Suspense fallback = {<div> Loading...</div>}>
+  <Routes>
+    <Route path = "/" element = {<Home/>}/>
+<Route path ="/about" element = {<About/>}/>
+  </Routes>
+  </Suspense>
+  </Router>
+);
+}
+export default App;
