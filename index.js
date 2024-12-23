f835cd33729f0a9d9763eba709e9de2f65ceeee5
Pankaj Chhawchharia <pankaj05011994@gmail.com>
2024-12-23 16:49:00 +0530

    Create index.js provide store to app

---
+++ index.js
@@ -0,0 +1,14 @@
+// Provide the store to App;
+
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider } from 'react-redux';
+
+import App from "./App";
+import Store from './Store';
+
+ReactDOM.render(
+  <Provider store = {Store}>
+  <App />
+  </Provider>,
+  document.getElementById("root"));
