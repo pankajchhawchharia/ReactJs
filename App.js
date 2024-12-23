fb8b377faff806d1be7d12fd16d5953f29b171c2
Pankaj Chhawchharia <pankaj05011994@gmail.com>
2024-12-23 16:48:06 +0530

    Create App.js import component

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ChildComponent from './ChildComponent';
+
+export default function App() {
+  return (
+    <>
+      <div>goal</div>
+      <ChildComponent />
+    </>
+  );
+}
